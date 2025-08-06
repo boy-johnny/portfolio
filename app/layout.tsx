01e025ef4547217f0009929c540ae7af933c864c
Johnny Boy <ddnT5521439SCo@gmail.com>
2025-08-06 16:54:18 +0800

    fix: fix the page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "GOODA | The Soruce of Creativity",
+  description: "Brian's portfolio, blog, and courses",
 };
 
 export default function RootLayout({
