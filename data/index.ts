1f62df674e5f43d752b421ebbd0868a363a89530
Johnny Boy <ddnT5521439SCo@gmail.com>
2025-08-06 14:21:50 +0800

    feat: update the content in work experience

--- data/index.ts
+++ data/index.ts
@@ -164,29 +164,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Frontend Engineer and Web Designer",
+    desc: "Built the Mitochondrial Research website from scratch using React.js.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Frontend Engineer & AI automation developer",
+    desc: "Developed the user-interface of e-commerce website using React.js, enchancing the efficiency of the website.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Full-stack Developer & Founder of GOODA",
+    desc: "Built a web-based SaaS app for AI optimization of photos.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Freelance Frontend Developer of ",
+    desc: "Developed and maintained user-facing features of open-source projects.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
